7cc65e49337857440ef3482145116278bbd83d44
Navid Shahnama <kingnavid@gmail.com>
2022-03-29 22:03:45 +0100

    Implement release API

---
+++ makeRelease.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+version=$1
+text=$2
+token=$3
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "tag_name": "$version",
+  "target_commitish": "$branch",
+  "name": "$version",
+  "body": "$text",
+  "draft": false,
+  "prerelease": false
+}
+EOF
+}
+
+echo "Create release $version for repo: $repo_full_name branch: $branch"
+
+    curl --data "$(generate_post_data)" \
+        --header "Accept:  application/vnd.github.v3+json" \
+        --header "Authorization: token $token" \
+        "https://api.github.com/repos/$repo_full_name/releases"
