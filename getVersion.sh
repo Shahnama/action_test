e9955ba31e48d7d0e887e0a0b19ac2e3f5651bc3
Navid Shahnama <kingnavid@gmail.com>
2022-03-29 22:03:07 +0100

    Implement release API

--- getVersion.sh
+++ getVersion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
    PROP_KEY=$1
-   PROP_VALUE=`cat ./version.properties  | grep ANDROID_VERSION_NAME | cut -d'=' -f2`
+   PROP_VALUE=`cat ./version.properties  | grep $PROP_KEY | cut -d'=' -f2`
    echo $PROP_VALUE
