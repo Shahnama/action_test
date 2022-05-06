2720808a5992edfed6a3b87ee2199f5134364353
Navid Shahnama <kingnavid@gmail.com>
2022-05-06 10:00:52 +0100

    add files

---
+++ upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+id=$1
+token=$2
+
+fileType="apk"
+searchFolder="./packages/**/*.$fileType"
+find . -type f -path "$searchFolder"
+foundFiles=$(find . -type f -path "$searchFolder")
+
+for file in $foundFiles ; do
+    echo "file:  ${file}"
+    echo "${file##*/}"
+
+echo
