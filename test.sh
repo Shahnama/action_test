bf431fc7d016d71ca67c7bb11083b4e62cf9e16a
Navid Shahnama <kingnavid@gmail.com>
2022-07-11 18:52:38 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,7 @@
 
 #!/usr/bin/env bash
 
-echo "FOO_ROOT: $FOO_ROOT"
-echo "FOO_JOB: $FOO_JOB"
-echo "FOO_STEP: $FOO_STEP"
+echo "in FOO_ROOT: $FOO_ROOT"
+echo "in FOO_JOB: $FOO_JOB"
+echo "in FOO_STEP: $FOO_STEP"
 echo " "
-printenv
